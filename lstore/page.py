30bfadfe45c8e3e2fa4781f33cd79c2e68400c90
Jocelyn Vuong <vuongjocelyn27@gmail.com>
2026-02-04 00:09:02 -0800

    finished defining write() and read()

--- lstore/page.py
+++ lstore/page.py
@@ -1,26 +1,43 @@
 import struct
 
-int_size = 8
-max_size = 4096 // int_size
+# each record is a 64-bit integer -> 8 bytes
+INT_SIZE = 8 
+# maximum number of records per page
+MAX_SIZE = 4096 // INT_SIZE
 
 class Page:
+    
 
     def __init__(self):
         self.num_records = 0
+        # allocate 4096 bytes of empty space
         self.data = bytearray(4096)
+        
 
     def has_capacity(self):
-        return self.num_records < max_size
+        return self.num_records < MAX_SIZE
+    
 
     def write(self, value):
-        if not self.has_capacity()
+        if not self.has_capacity():
             raise RuntimeError("Page is full")
         
+        # compute the start index to insert new record at
+        start = self.num_records * INT_SIZE
+        # insert the integer as 8 bytes with order of bytes being least significant first
+        self.data[start:start + INT_SIZE] = value.to_bytes(INT_SIZE, byteorder = 'little')
+        
         self.num_records += 1
-        pass
-
+        
+        
     def read(self, value):
-        pass
+        if value < 0 or value >= self.num_records:
+            raise RuntimeError("Index out of bounds")
+        
+        start = value * INT_SIZE
+        return int.from_bytes(self.data[start:start + INT_SIZE], byteorder='little')
+    
 
+    # might not need for milestone 1
     def update(self, value):
         pass
